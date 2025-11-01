f492076283e38b5ce6bc750084ffb33397b62a65
shauryasharmarf <shauryasharmarf@gmail.com>
2025-11-01 22:01:06 +0530

    Add files via upload

---
+++ myfirstpython.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print("Addition:",sum)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
